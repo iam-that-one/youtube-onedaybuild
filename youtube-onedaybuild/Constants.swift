b47dc4ee1f3382108a07610ec92bcad7d5508bb3
iam-that-one <iam-that-one@users.noreply.github.com>
2020-11-27 03:58:37 +0300

    Test

---
+++ youtube-onedaybuild/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  youtube-onedaybuild
+//
+//  Created by Abdullah Alnutayfi on 27/11/2020.
+//
+
+import Foundation
+struct Constants {
+    static var API_KEY = "AIzaSyDeeOX8hJy3ABddMFXXhiBFWQQoancRIXQ"
+    static var PLAYLIST_ID = "PLMRqhzcHGw1aLoz4pM_Mg2TewmJcMg9ua"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+}
+
